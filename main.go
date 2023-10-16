ee8ed8295dfe31d2199e4ee4efb47cc2909a535e
정승현 <tmdgusya@naver.com>
2023-10-16 21:53:36 +0900

    Using waitGroup

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func printSomething(s string) {
+func printSomething(s string, group *sync.WaitGroup) {
+	defer group.Done()
 	fmt.Println(s)
 }
 
 // one go routine
 // program has one goroutine
 func main() {
-	go printSomething("This is first thing to be printed")
-	printSomething("This is second thing to be printed")
+	var waitGroup sync.WaitGroup
+
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+	}
+
+	waitGroup.Add(len(words))
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d:%s", i, x), &waitGroup)
+	}
+
+	defer waitGroup.Wait()
+
+	printSomething("This is second thing to be printed", &waitGroup)
 }
